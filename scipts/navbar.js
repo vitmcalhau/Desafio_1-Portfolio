7a041fd60c0b160fafefd1ea589a69ae0892dbb2
Vítor de Melo Calhau <v248740@dac.unicamp.br>
2024-03-22 22:42:19 -0300

    fix: melhora responsividade do menu e elementos da página principal

--- scipts/navbar.js
+++ scipts/navbar.js
@@ -11,3 +11,9 @@ if (currentScroll > lastScroll) {
 }
 lastScroll = currentScroll <= 0 ? 0 : currentScroll;
 }, false);
+
+window.onload = () => {
+    document.querySelector('.menu-compacto').addEventListener('click', function() {
+        document.querySelector('.menu-lista').classList.toggle('dropdown');
+    });
+}
